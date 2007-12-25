83e0bc20cd4af6b3b5e225a38ee3970107d590a5
oh2mqk <oh2mqk@7f334e7d-9667-46a4-86d4-45028e631dbf>
2007-12-25 01:42:46 +0000

    missed new files

---
+++ aprx-stat.c
@@ -0,0 +1,162 @@
+/* **************************************************************** *
+ *                                                                  *
+ *  APRX -- 2nd generation receive-only APRS-i-gate with            *
+ *          minimal requirement of esoteric facilities or           *
+ *          libraries of any kind beyond UNIX system libc.          *
+ *                                                                  *
+ * (c) Matti Aarnio - OH2MQK,  2007                                 *
+ *                                                                  *
+ * **************************************************************** */
+
+
+#include "aprx.h"
+
+time_t now;
+int erlangout;
+char *aprxlogfile;
+
+void erlang_snmp(void)
+{
+	int i;
+
+	/* SNMP data output - continuously growing counters
+	 */
+
+	for (i = 0; i < ErlangLinesCount; ++i) {
+	  struct erlangline *E = ErlangLines[i];
+
+	  printf("%s   %ld %ld   %ld  %ld     %d\n",
+		 E->name,
+		 E->SNMP.bytes_rx, E->SNMP.packets_rx,
+		 E->SNMP.bytes_tx, E->SNMP.packets_tx,
+		 (int)(now - E->last_update));
+	}
+}
+
+void erlang_xml(int topmode)
+{
+	int i, j, k, t;
+
+	/* What this outputs is not XML, but a mild approximation
+	   of the data that XML version would output.. 
+	   It is not even the whole dataset, just last 60 samples
+	   of each type.
+	*/
+
+	for (i = 0; i < ErlangLinesCount; ++i) {
+	  struct erlangline *E = ErlangLines[i];
+	  char logtime[40];
+	  struct tm *wallclock;
+
+	  printf("\nSNMP  %s   %ld %ld   %ld  %ld     %d\n",
+		 E->name,
+		 E->SNMP.bytes_rx, E->SNMP.packets_rx,
+		 E->SNMP.bytes_tx, E->SNMP.packets_tx,
+		 (int)(now - E->last_update));
+
+	  printf("\n1min data\n");
+	  k = E->e1_cursor;
+	  t = E->e1_max; if (topmode) t = 90;
+	  for (j = 0; j < t; ++j) {
+	    --k; if (k < 0) k = E->e1_max-1;
+	    if (E->e1[k].update == 0) continue;
+	    wallclock = gmtime(&E->e1[k].update);
+	    strftime(logtime, sizeof(logtime), "%Y-%m-%d %H:%M", wallclock);
+	    printf("%s  %s %2dm  %5ld  %3ld  %5ld  %3ld   %5.3f  %5.3f\n",
+		   logtime, E->name, 1,
+		   E->e1[k].bytes_rx,  E->e1[k].packets_rx,
+		   E->e1[k].bytes_tx,  E->e1[k].packets_tx,
+		   (float)E->e1[k].bytes_rx/(float)E->erlang_capa,
+		   (float)E->e1[k].bytes_tx/(float)E->erlang_capa);
+	  }
+
+
+	  printf("\n10min data\n");
+	  k = E->e10_cursor;
+	  t = E->e10_max; if (topmode) t = 10;
+	  for (j = 0; j < t; ++j) {
+	    --k; if (k < 0) k = E->e10_max-1;
+	    if (E->e10[k].update == 0) continue;
+	    wallclock = gmtime(&E->e10[k].update);
+	    strftime(logtime, sizeof(logtime), "%Y-%m-%d %H:%M", wallclock);
+	    printf("%s  %s %2dm  %5ld  %3ld  %5ld  %3ld   %5.3f  %5.3f\n",
+		   logtime, E->name, 10,
+		   E->e10[k].bytes_rx,  E->e10[k].packets_rx,
+		   E->e10[k].bytes_tx,  E->e10[k].packets_tx,
+		   (float)E->e10[k].bytes_rx/((float)E->erlang_capa*10.0),
+		   (float)E->e10[k].bytes_tx/((float)E->erlang_capa*10.0));
+	  }
+
+
+	  printf("\n60min data\n");
+	  k = E->e60_cursor;
+	  t = E->e60_max; if (topmode) t = 3;
+	  for (j = 0; j < t; ++j) {
+	    --k; if (k < 0) k = E->e60_max-1;
+	    if (E->e60[k].update == 0) continue;
+	    wallclock = gmtime(&E->e60[k].update);
+	    strftime(logtime, sizeof(logtime), "%Y-%m-%d %H:%M", wallclock);
+	    printf("%s  %s %2dm  %5ld  %3ld  %5ld  %3ld   %5.3f  %5.3f\n",
+		   logtime, E->name, 60,
+		   E->e60[k].bytes_rx,  E->e60[k].packets_rx,
+		   E->e60[k].bytes_tx,  E->e60[k].packets_tx,
+		   (float)E->e60[k].bytes_rx/((float)E->erlang_capa*60.0),
+		   (float)E->e60[k].bytes_tx/((float)E->erlang_capa*60.0));
+	  }
+
+
+
+	}
+
+
+	exit(0);
+}
+
+
+void usage(void)
+{
+	printf("Usage: aprx-stat [-f arpx-erlang.dat] {-S|-x|-X}\n");
+	exit(64);
+}
+
+int main(int argc, char **argv)
+{
+	int opt;
+	int mode_snmp = 0;
+	int mode_xml  = 0;
+
+	now = time(NULL);
+
+	while ((opt = getopt(argc, argv, "f:SxX?h")) != -1) {
+	  switch (opt) {
+	  case 'f':
+	    erlang_backingstore = optarg;
+	    break;
+	  case 'S': /* SNMP */
+	    ++mode_snmp;
+	    break;
+	  case 'X':
+	    mode_xml = 1;
+	    break;
+	  case 'x':
+	    mode_xml = 2;
+	    break;
+	  default:
+	    usage();
+	    break;
+	  }
+	}
+
+	erlang_start(0); /* Open the backing-store */
+
+	if (mode_snmp) {
+	  erlang_snmp();
+	} else if (mode_xml == 1) {
+	  erlang_xml(0);
+	} else if (mode_xml == 2) {
+	  erlang_xml(1);
+	} else
+	  usage();
+
+	return 0;
+}
