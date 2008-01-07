dde1f2df9e671ade03b5a5e2f9b97fa08c9908ad
oh2mqk <oh2mqk@7f334e7d-9667-46a4-86d4-45028e631dbf>
2008-01-07 22:54:00 +0000

    tags&trunks..  version 0.15

---
+++ tags/version-0.15/trunk/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+static int aprspass(const char *mycall)
+{
+	int a = 0, h = 29666, c;
+
+	for ( ; *mycall ; ++mycall ) {
+	  c = 0xFF & *mycall;
+	  if (!(( '0' <= c && c <= '9' ) ||
+		( 'A' <= c && c <= 'Z' )))
+	    break;
+	  h ^= ((0xFF & *mycall) * (a ? 1 : 256));
+	  a = !a;
+	}
+	return h;
+}
+
+main() {
+  printf("APRSPASS: %d\n",aprspass("OH2MQK-1"));
+  return 0;
+}
