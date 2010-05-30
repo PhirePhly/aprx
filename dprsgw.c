6749122e2c38277ae7f36c15a6e0a2bc0d36baf3
oh2mqk <oh2mqk@7f334e7d-9667-46a4-86d4-45028e631dbf>
2010-05-30 00:18:20 +0000

    DPRS GPS-A type packet Rx-iGate processing

--- dprsgw.c
+++ dprsgw.c
@@ -36,30 +36,33 @@ void *dprsgw_new( void )
 }
 
 
-void dprslog( const char *buf ) {
+void dprslog( const time_t stamp, const char *buf ) {
   FILE *fp = fopen("/tmp/dprslog.txt","a");
 
-  fprintf(fp, "%ld\t%s\n", now, buf);
+  fprintf(fp, "%ld\t%s\n", stamp, buf);
 
   fclose(fp);
 }
 
-// "Specification" says to use this checksum..
-// But this is just horribly inefficient way to calculate
-// CCITT-CRC-16 -- for which we have crc16_calc()
-static int dprsgw_crccheck( const char *s, int len )
+// The "Specification" says to use this checksum..
+// It uses right-left inverted version of the polynome
+// of CCITT-CRC-16 but processing is not compatible..
+// Thus the result is NOT CCITT-CRC-16, but something
+// uniquely ICOM D-STAR..
+
+static int dprsgw_crccheck( const uint8_t *s, int len )
 {
 	int icomcrc = 0xffff;
 	for ( ; len > 0; ++s, --len) {
-	  int ch = (*s) & 0xff;
+	  uint8_t ch = *s;
 	  int i;
 	  for (i = 0; i < 8; i++) {
-	      int xorflag = (((icomcrc ^ ch) & 0x01) == 0x01);
-	      icomcrc >>= 1;
-	      if (xorflag)
-		icomcrc ^= 0x8408;
-	      ch >>= 1;
-	    }
+	    int xorflag = (icomcrc ^ ch) & 0x01;
+	    icomcrc >>= 1;
+	    if (xorflag)
+	      icomcrc ^= 0x8408;
+	    ch >>= 1;
+	  }
 	}
 	return (~icomcrc) & 0xffff;
 }
@@ -75,15 +78,21 @@ static int dprsgw_isvalid( struct serialport *S )
 
 	if (memcmp("$$CRC", S->rdline, 5) == 0 && S->rdline[9] == ',') {
 	  // Maybe a $$CRCB727,OH3BK-D>APRATS,DSTAR*:@165340h6128.23N/02353.52E-D-RATS (GPS-A) /A=000377
-	  int crc = crc16_calc(S->rdline+10, S->rdlinelen-10);
+	  int crc;
 	  int csum = -1;
+
+	  S->rdline[S->rdlinelen] = '\r';
+	  crc = dprsgw_crccheck(S->rdline+10, S->rdlinelen+1-10); // INCLUDE the CR on CRC calculation!
+	  S->rdline[S->rdlinelen] = 0;
 	  i = sscanf(S->rdline, "$$CRC%04x,", &csum);
 	  if (csum != crc) {
-	    if (debug) printf("Bad DPRS APRS CRC: %04x vs. %s", crc, S->rdline);
-	    return 0;
+	    if (debug) printf("Bad DPRS APRS CRC: l=%d, i=%d, %04x/%04x vs. %s\n", S->rdlinelen, i, crc, csum, S->rdline);
+	    // return 0;
+	  } else {
+	    if (debug) printf("Good DPRS APRS CRC: l=%d, i=%d, %04x/%04x vs. %s\n", S->rdlinelen, i, crc, csum, S->rdline);
+	    return 1;
 	  }
-	  if (debug) printf("DPRS APRS packet is OK: %s\n", S->rdline+10);
-	  return 1;
+	  return 0;
 
 	} else if (memcmp("$GP", S->rdline, 3) == 0) {
 	  // Maybe  $GPRMC,170130.02,A,6131.6583,N,02339.1552,E,0.00,154.8,290510,6.5,E,A*02  ?
@@ -115,6 +124,56 @@ static int dprsgw_isvalid( struct serialport *S )
 			      S->rdlinelen, S->rdline);
 	    return 0;
 	  }
+	  return 1; // Maybe valid?
+	}
+}
+
+static void dprsgw_rxigate( struct serialport *S )
+{
+	int i;
+	const struct aprx_interface *aif = S->interface[0];
+	char *tnc2addr = S->rdline;
+	int   tnc2addrlen = S->rdlinelen;
+	char *tnc2body = S->rdline;
+	int   tnc2bodylen = S->rdlinelen;
+
+#ifndef DPRSGW_DEBUG_MAIN
+	if (aif == NULL) {
+	  if (debug) printf("OOPS! NO <interface> ON DPRS SERIAL PORT! BUG!\n");
+	  return;
+	}
+#endif
+
+	if (memcmp("$$CRC", tnc2addr, 5) == 0 && tnc2addrlen > 20) {
+	  tnc2addr += 10;
+	  tnc2addrlen -= 10;
+	  tnc2bodylen -= 10;
+	  tnc2body = memchr( tnc2addr, ':', tnc2addrlen);
+	  if (tnc2body != NULL) {
+	    tnc2addrlen = tnc2body - tnc2addr;
+	    tnc2body += 1;
+
+	    // MAYBE RATELIMIT
+	    // Acceptable packet, Rx-iGate it!
+	    igate_to_aprsis( aif->callsign, 0, tnc2addr, tnc2addrlen, tnc2bodylen, 0);
+
+/*
+	    // MUST RATELIMIT!
+	    interface_receive_3rdparty( aif,
+					fromcall, origtocall,
+					b, ax25len - (b-ax25) );
+*/
+	    return;
+	    
+	  } else {
+	    // Bad packet!
+	    if (debug) printf("Bad packet! %s\n", S->rdline);
+	    return;
+	  }
+
+	} else {
+	  if (debug) printf("Unsupported packet! %s\n", S->rdline);
+	  return;
 	}
 }
 
@@ -132,11 +191,32 @@ static void dprsgw_receive( struct serialport *S )
 {
 	int i;
 
-	dprslog(S->rdline);
+	dprslog(S->rdline_time, S->rdline);
 
-	if (dprsgw_isvalid(S)) {
-	  // FIXME: Feed it to DPRS-APRS-GATE
-	}
+	do {
+	  if (dprsgw_isvalid(S)) {
+	    // Feed it to DPRS-APRS-GATE
+	    if (memcmp("$$CRC", S->rdline, 5) == 0) {
+	      // Valid GPS-A mode packet!
+	      dprsgw_rxigate( S );
+	      return; // Done!
+	    }
+	    // FIXME: Other packet formats!
+	    return; // Done!
+	  } else {
+	    // Not a good packet! See if there is a good packet inside?
+	    uint8_t *p = memchr(S->rdline+1, '$', S->rdlinelen-1);
+	    if (p == NULL)
+	      break; // No '$' to start something
+	    i = S->rdlinelen - (p - S->rdline);
+	    if (i <= 0)
+	      break; // exhausted!
+	    S->rdlinelen = i;
+	    memcpy(S->rdline, p, S->rdlinelen);
+	    S->rdline[i] = 0;
+	    continue;
+	  }
+	} while(1);
 }
 
 
@@ -155,7 +235,7 @@ int dprsgw_pulldprs( struct serialport *S )
 
 		S->rdline[S->rdlinelen] = 0;
 		if (S->rdlinelen > 0)
-		  dprslog(S->rdline);
+		  dprslog(rdtime, S->rdline);
 
 		S->rdlinelen = 0;
 		S->dprsstate = DPRSSTATE_SYNCHUNT;
@@ -202,6 +282,7 @@ int dprsgw_pulldprs( struct serialport *S )
 					  break; // exhausted
 					}
 					memcpy(S->rdline, p, len);
+					S->rdline[len] = 0;
 					S->rdlinelen = len;
 					if (len >= 3) {
 					  if (memcmp("$$C", S->rdline, 3) != 0 &&
@@ -226,6 +307,7 @@ int dprsgw_pulldprs( struct serialport *S )
 				// No correct start, discard...
 				S->rdlinelen = 2;
 				memcpy(S->rdline, S->rdline+1, 2);
+				S->rdline[S->rdlinelen] = 0;
 				if (S->rdline[0] != '$') {
 					// Didn't start with a '$'
 					S->rdlinelen = 0;
@@ -275,6 +357,7 @@ int dprsgw_pulldprs( struct serialport *S )
 				  break; // exhausted
 				}
 				memcpy(S->rdline, p, len);
+				S->rdline[len] = 0;
 				S->rdlinelen = len;
 				if (len >= 3) {
 				  if (memcmp("$$C", S->rdline, 3) != 0 &&
@@ -307,3 +390,37 @@ int  dprsgw_postpoll(struct aprxpolls *app)
 	return 0;
 }
 
+
+
+#ifdef DPRSGW_DEBUG_MAIN
+int debug = 1;
+time_t now;
+int main(int argc, char *argv[]) {
+  struct serialport S;
+  // A test where string has initially some incomplete data, then finally a real data
+  printf("\nFIRST TEST\n");
+  strcpy(S.rdline, "x$x4$GPPP$$$GP  $$CRCB727,OH3BK-D>$$CRCB727,OH3BK-D>APRATS,DSTAR*:@165340h6128.23N/02353.52E-D-RATS (GPS-A) /A=000377");
+  S.rdlinelen = strlen(S.rdline);
+  dprsgw_receive(&S);
+
+  printf("\nSECOND TEST\n");
+  strcpy(S.rdline, "\304\3559\202\333$$CRCC3F5,OH3KGR-M>API282,DSTAR*:/123035h6131.29N/02340.45E>/IC-E2820");
+  S.rdlinelen = strlen(S.rdline);
+  dprsgw_receive(&S);
+
+  printf("\nTHIRD TEST\n");
+  strcpy(S.rdline, "[SOB]\"=@=@=@=>7\310=@\010!~~~~~~~!~~~~~~~\001\001\001\001\001\001\001\001[EOB]$$CRCBFB7,OH3BK>APRATS,DSTAR*:@124202h6128.23N/02353.52E-D-RATS (GPS-A) /A=000377");
+  S.rdlinelen = strlen(S.rdline);
+  dprsgw_receive(&S);
+
+  return 0;
+}
+
+int ttyreader_getc(struct serialport *S)
+{
+}
+void igate_to_aprsis(const char *portname, const int tncid, const char *tnc2buf, int tnc2addrlen, int tnc2len, const int discard)
+{
+  printf("RX-IGATE: %s\n", tnc2buf);
+}
+#endif
