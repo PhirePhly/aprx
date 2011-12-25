5fe7972d5e62d0026b978ac9831bf80b39125881
oh2mqk <oh2mqk@7f334e7d-9667-46a4-86d4-45028e631dbf>
2011-12-25 21:25:36 +0000

    Publish version 2.03 test1 for beta testers.

--- trunk/aprx.c
+++ trunk/aprx.c
@@ -26,8 +26,8 @@ const char *rflogfile;
 const char *aprxlogfile;
 const char *mycall;
 
-const char *tocall = "APRX22";
-const uint8_t tocall25[7] = {'A'<<1,'P'<<1,'R'<<1,'X'<<1,'2'<<1,'2'<<1,0x60};
+const char *tocall = "APRX23";
+const uint8_t tocall25[7] = {'A'<<1,'P'<<1,'R'<<1,'X'<<1,'2'<<1,'3'<<1,0x60};
 
 #ifndef CFGFILE
 #define CFGFILE "/etc/aprx.conf"
