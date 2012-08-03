edf00e1a1ce59741a8d9372e2dbac522c20b4b00
oh2mqk <oh2mqk@7f334e7d-9667-46a4-86d4-45028e631dbf>
2012-08-03 19:57:14 +0000

    filter_entrycall_insert(): Make sure the callsign key length fits in the buffer. Same for (unused) filter_wx_insert().

--- filter.c
+++ filter.c
@@ -310,7 +310,7 @@ int filter_entrycall_insert(struct pbuf_t *pb)
 	const char *key = pb->qconst_start+4;
         char uckey[CALLSIGNLEN_MAX+1];
 
-	for (keylen = 0; keylen <= CALLSIGNLEN_MAX; ++keylen) {
+	for (keylen = 0; keylen < CALLSIGNLEN_MAX; ++keylen) {
 		int c = key[keylen];
 		if (c == ',' || c == ':')
 			break;
@@ -519,7 +519,7 @@ int filter_wx_insert(struct pbuf_t *pb)
 	if (!((pb->packettype & T_WX) && !(pb->flags & F_HASPOS)))
 		return 0;
 
-	for (idx = 0; idx <= keylen; ++idx) {
+	for (idx = 0; idx <= keylen && idx < CALLSIGNLEN_MAX; ++idx) {
 		int c = key[idx];
 		if (c == ',' || c == ':')
 			break;
